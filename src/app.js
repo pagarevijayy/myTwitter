7c166cc01869166a8470ebab2c7256efc5870024
pagarevijayy <pagarevj97@gmail.com>
2019-07-30 17:05:05 +0530

    updated package.json and db/mongoose.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+require('./db/mongoose')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+//Server connection
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}..`);
+})
