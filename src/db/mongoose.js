dc7db7882186755733d1d8a29c60b07402ca9f5f
Abhishek Nair <abhisheknair4869@gmail.com>
2019-08-18 21:05:28 +0530

    added env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/myTwitter', {
-     useNewUrlParser: true,
-     useCreateIndex: true,
-     useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
